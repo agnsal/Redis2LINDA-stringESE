87ab727b63119cc0e4fe35c1d525326dcf1baecf
Agnese Salutari <agneses92@hotmail.it>
2018-01-12 17:55:54 +0100

    Update Redis2LINDA.py

--- LindaProxy/Redis2LINDA.py
+++ LindaProxy/Redis2LINDA.py
@@ -1,12 +1,15 @@
+
 '''
-Example of Linda Proxy in Python
+Copyright 2017-2018 Agnese Salutari.
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at
 
- Licensed with Apache Public License
- by AAAI Research Group
- Department of Information Engineering and Computer Science and Mathematics
- University of L'Aquila, ITALY
- http://www.disim.univaq.it
+http://www.apache.org/licenses/LICENSE-2.0
 
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on 
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and limitations under the License
 '''
 
 import LindaProxy.lindaproxy as lp
